f65f421c7bca32f8802ee503e13dd5cdbc10496d
BillOchieng <ochieng01@allegheny.edu>
2022-10-27 13:39:44 -0400

    changed site

--- site/.eleventy.js
+++ site/.eleventy.js
@@ -1,52 +1,101 @@
-const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
-const imageShortcode = require('./src/_11ty/shortcodes/image-shortcode');
-const markdownLibrary = require('./src/_11ty/libraries/markdown-library');
-const minifyHtml = require('./src/_11ty/utils/minify-html');
-const markdownFilter = require('./src/_11ty/filters/markdown-filter');
-const browserSyncConfig = require('./src/_11ty/utils/browser-sync-config');
-const { readableDateFilter, machineDateFilter } = require('./src/_11ty/filters/date-filters');
+const { DateTime } = require("luxon");
+const fs = require("fs");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const pluginNavigation = require("@11ty/eleventy-navigation");
+const markdownIt = require("markdown-it");
+const markdownItAnchor = require("markdown-it-anchor");
 
-module.exports = function (eleventyConfig) {
-  // Plugins
-  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.addPlugin(pluginSyntaxHighlight);
+  eleventyConfig.addPlugin(pluginNavigation);
 
-  // Filters
-  eleventyConfig.addFilter('markdown', markdownFilter);
-  eleventyConfig.addFilter('readableDate', readableDateFilter);
-  eleventyConfig.addFilter('machineDate', machineDateFilter);
-
-  // Shortcodes
-  eleventyConfig.addNunjucksAsyncShortcode('Image', imageShortcode);
-
-  // Libraries
-  eleventyConfig.setLibrary('md', markdownLibrary);
-
-  // Merge data instead of overriding
   eleventyConfig.setDataDeepMerge(true);
 
-  // Trigger a build when files in this directory change
-  eleventyConfig.addWatchTarget('./src/assets/scss/');
+  eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
 
-  // Minify HTML output
-  eleventyConfig.addTransform('htmlmin', minifyHtml);
+  eleventyConfig.addFilter("readableDate", dateObj => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
+  });
 
-  // Don't process folders with static assets
-  eleventyConfig.addPassthroughCopy('./src/favicon.ico');
-  eleventyConfig.addPassthroughCopy('./src/admin');
-  eleventyConfig.addPassthroughCopy('./src/assets/img');
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+  });
 
-  // Allow Turbolinks to work in development mode
-  eleventyConfig.setBrowserSyncConfig(browserSyncConfig);
+  // Get the first `n` elements of a collection.
+  eleventyConfig.addFilter("head", (array, n) => {
+    if( n < 0 ) {
+      return array.slice(n);
+    }
+
+    return array.slice(0, n);
+  });
+
+  eleventyConfig.addCollection("tagList", require("./_11ty/getTagList"));
+
+  eleventyConfig.addPassthroughCopy("img");
+  eleventyConfig.addPassthroughCopy("files");
+  eleventyConfig.addPassthroughCopy("css");
+
+  /* Markdown Overrides */
+  let markdownLibrary = markdownIt({
+    html: true,
+    breaks: true,
+    linkify: true
+  }).use(markdownItAnchor, {
+    permalink: true,
+    permalinkClass: "direct-link",
+    permalinkSymbol: "#"
+  });
+  eleventyConfig.setLibrary("md", markdownLibrary);
+
+  // Browsersync Overrides
+  eleventyConfig.setBrowserSyncConfig({
+    callbacks: {
+      ready: function(err, browserSync) {
+        const content_404 = fs.readFileSync('_site/404.html');
+
+        browserSync.addMiddleware("*", (req, res) => {
+          // Provides the 404 content without redirect.
+          res.write(content_404);
+          res.end();
+        });
+      },
+    },
+    ui: false,
+    ghostMode: false
+  });
 
   return {
-    templateFormats: ['md', 'njk', 'html'],
-    markdownTemplateEngine: 'njk',
-    htmlTemplateEngine: 'njk',
-    dataTemplateEngine: 'njk',
-    passthroughFileCopy: true,
+    templateFormats: [
+      "md",
+      "njk",
+      "html",
+      "liquid"
+    ],
+
+    // If your site lives in a different subdirectory, change this.
+    // Leading or trailing slashes are all normalized away, so don’t worry about those.
+
+    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
+    // This is only used for link URLs (it does not affect your file structure)
+    // Best paired with the `url` filter: https://www.11ty.io/docs/filters/url/
+
+    // You can also pass this in on the command line using `--pathprefix`
+    // pathPrefix: "/",
+
+    markdownTemplateEngine: "liquid",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+
+    // These are all optional, defaults are shown:
     dir: {
-      input: 'src',
-      layouts: "_layouts"
-    },
+      input: ".",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
+    }
   };
 };
